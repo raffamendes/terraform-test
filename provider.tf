04d8a4dfbb8aa39674abdf3845fa349f9ff0761a
raffamendes <raffael.pikachu@gmail.com>
2019-11-20 22:05:57 -0300

    Update provider

--- provider.tf
+++ provider.tf
@@ -1,8 +1,3 @@
 # Configure credential OpenStack Provider
-provider "openstack" {
-  user_name   = "my-litle-user"
-  tenant_name = "my-little-tenant"
-  password    = "secret"
-  auth_url    = "http://your-cloud-prodivder.com"
-}
+provider "openstack" {}
 
