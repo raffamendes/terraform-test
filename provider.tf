300384967c234a28b39cc4b0f9db76497cfeeb04
Raffael Correia Mendes <raffael.pikachu@gmail.com>
2019-11-25 09:12:05 -0300

    Update provider

--- provider.tf
+++ provider.tf
@@ -1,3 +1,9 @@
 # Configure credential OpenStack Provider
-provider "openstack" {}
+provider "openstack" {
+  user_name   = "rmendes-osp"
+  tenant_name = "rmendes-osp_project"
+  password    = "redhat"
+  auth_url    = "https://10.37.197.10:13000"  
+  insecure    = true
+}
 
